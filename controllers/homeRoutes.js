69431fd01c9fb8289a9e2a11e2bdac83fb6f9303
sahana <sahanamai@gmail.com>
2022-02-24 22:13:37 -0600

    post/id route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const{User,Post,Comment} = require("../models/");
-router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {//homepage route
   console.log("jjj")
     try {
      var postData=await Post.findAll({
@@ -32,6 +32,28 @@ router.get('/', async (req, res) => {
     res.render('login');
   });
 
+//post route-homepage
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
