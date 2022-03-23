27a7ea8edefcd9dad3746982e16b4110d88e138a
sahana <sahanamai@gmail.com>
2022-03-23 12:45:41 -0500

    added comments

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+// Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
